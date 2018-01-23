0c3d9f80beb629cdbf2630bb57543b6d718a5591
Freddie <freddie.romero23@yahoo.com>
2018-01-22 22:43:02 -0700

    Modified the command line argument iteration code in main method to use the for-each syntax.

--- src/edu/cnm/deepdive/helloworld/HelloWorld.java
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -13,8 +13,8 @@ public class HelloWorld {
     String message = String.format("Hello %s!", target);
     emitMessage(message);
     
-    for (int i = 0; i < args.length; i++) {
-      System.out.printf("Arg # %d is %s%n", i, args[i]); 
+    for (String arg : args) {
+      System.out.printf("Argument: %s%n", arg);
       
     }
     
