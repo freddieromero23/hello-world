291b6b1cc84804cf55f17eabb4e498df97387258
Freddie <freddie.romero23@yahoo.com>
2018-01-22 22:28:06 -0700

    Invoked the System.out.printf method to print the counter and the corresponding argument for each command line argument.

--- src/edu/cnm/deepdive/helloworld/HelloWorld.java
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -11,8 +11,12 @@ public class HelloWorld {
   public static void main(String[] args) {
     String target = ((args.length > 0) ? args[0] : "World");
     String message = String.format("Hello %s!", target);
-    
     emitMessage(message);
+    
+    for (int i = 0; i < args.length; i++) {
+      System.out.printf("Arg # %d is %s%n", i, args[i]); 
+      
+    }
     
   }
   
